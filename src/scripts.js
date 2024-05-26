1c2d9226c1f38882d728f27c34192c5ec68ea9c8
leoradavid <liuleora@gmail.com>
2024-05-27 00:56:11 +0900

    Final update

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,95 @@
 function handleSearch(query) {
+    saveAudioState();
     window.location.href = `characters.html?search=${query}`;
 }
+
+function handleThemeChange(theme) {
+    document.body.className = theme;
+    localStorage.setItem('selectedTheme', theme);
+    playThemeMusic(theme);
+}
+
+function playThemeMusic(theme) {
+    const audioElement = document.getElementById('backgroundMusic');
+    switch (theme) {
+        case 'classic':
+            audioElement.src = 'https://disneycharacter.s3.amazonaws.com/bgms/Tangled_ISeeTheLight.mp3';
+            break;
+        case 'tangled':
+            audioElement.src = 'https://disneycharacter.s3.amazonaws.com/bgms/Tangled_WhenWillMyLifeBegin.mp3';
+            break;
+        case 'frozen':
+            audioElement.src = 'https://disneycharacter.s3.amazonaws.com/bgms/Frozen_LetItGo.mp3';
+            break;
+        case 'the-little-mermaid':
+            audioElement.src = 'https://disneycharacter.s3.amazonaws.com/bgms/TheLittleMermaid_PartOfYourWorld.mp3';
+            break;
+        default:
+            audioElement.src = '';
+    }
+    if (audioElement.src) {
+        audioElement.play();
+    }
+}
+
+function setThemeBackground(theme) {
+    const backgrounds = {
+        'classic': 'url(\'https://disneycharacter.s3.amazonaws.com/img/6.jpg\')',
+        'tangled': 'url(\'https://disneycharacter.s3.amazonaws.com/img/7.jpg\')',
+        'frozen': 'url(\'https://disneycharacter.s3.amazonaws.com/img/5.jpg\')',
+        'the-little-mermaid': 'url(\'https://disneycharacter.s3.amazonaws.com/img/4.jpg\')'
+    };
+    document.body.style.backgroundImage = backgrounds[theme] || '';
+}
+
+function handleMusicChange(music) {
+    const audioElement = document.getElementById('backgroundMusic');
+    switch (music) {
+        case 'frozen':
+            audioElement.src = 'https://disneycharacter.s3.amazonaws.com/bgms/Frozen_LetItGo.mp3';
+            break;
+        case 'tangled-one':
+            audioElement.src = 'https://disneycharacter.s3.amazonaws.com/bgms/Tangled_ISeeTheLight.mp3';
+            break;
+        case 'tangled-two':
+            audioElement.src = 'https://disneycharacter.s3.amazonaws.com/bgms/Tangled_WhenWillMyLifeBegin.mp3';
+            break;
+        case 'the-little-mermaid':
+            audioElement.src = 'https://disneycharacter.s3.amazonaws.com/bgms/TheLittleMermaid_PartOfYourWorld.mp3';
+            break;
+        default:
+            audioElement.src = '';
+    }
+    localStorage.setItem('selectedMusic', music);
+    if (audioElement.src) {
+        audioElement.play();
+    }
+}
+
+function setDefaults() {
+    const savedTheme = localStorage.getItem('selectedTheme');
+    if (savedTheme) {
+        document.body.className = savedTheme;
+        document.getElementById('themeSelect').value = savedTheme;
+        setThemeBackground(savedTheme);
+        playThemeMusic(savedTheme);
+    } else {
+        handleThemeChange('classic');
+    }
+
+    const savedMusic = localStorage.getItem('selectedMusic');
+    if (savedMusic) {
+        document.getElementById('musicSelect').value = savedMusic;
+        handleMusicChange(savedMusic);
+    } else {
+        handleMusicChange('tangled-one');
+    }
+
+    restoreAudioState();
+}
+
+window.addEventListener('beforeunload', saveAudioState);
+window.addEventListener('load', setDefaults);
 
 function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
@@ -17,7 +106,13 @@ window.addEventListener('load', () => {
 });
 
 document.getElementById('themeSelect').addEventListener('change', function() {
-    document.body.className = this.value;
+    const theme = this.value;
+    handleThemeChange(theme);
+    setThemeBackground(theme);
+});
+
+document.getElementById('musicSelect').addEventListener('change', function() {
+    handleMusicChange(this.value);
 });
 
 document.getElementById('headerSearchButton').addEventListener('click', () => {
@@ -183,3 +278,29 @@ document.getElementById('stopButton').addEventListener('click', function() {
 volumeControl.addEventListener('input', function() {
     audioElement.volume = volumeControl.value;
 });
+
+function saveAudioState() {
+    const audioElement = document.getElementById('backgroundMusic');
+    sessionStorage.setItem('audioSrc', audioElement.src);
+    sessionStorage.setItem('audioCurrentTime', audioElement.currentTime);
+    sessionStorage.setItem('audioIsPlaying', !audioElement.paused);
+}
+
+function restoreAudioState() {
+    const audioElement = document.getElementById('backgroundMusic');
+    const audioSrc = sessionStorage.getItem('audioSrc');
+    const audioCurrentTime = sessionStorage.getItem('audioCurrentTime');
+    const audioIsPlaying = sessionStorage.getItem('audioIsPlaying') === 'true';
+
+    if (audioSrc) {
+        audioElement.src = audioSrc;
+        audioElement.currentTime = audioCurrentTime ? parseFloat(audioCurrentTime) : 0;
+        if (audioIsPlaying) {
+            audioElement.play().then(() => {
+                audioElement.play();
+            }).catch(error => {
+                console.error('Error resuming audio playback:', error);
+            });
+        }
+    }
+}
